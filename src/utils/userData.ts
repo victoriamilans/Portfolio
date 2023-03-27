d35d7965c4245b765c2c8b5a87d7f8f8d146ce2c
Victoria <victoria-milan@outlook.com>
2023-03-27 10:21:36 -0300

    feat/ change userData

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
+  nameUser: "Victoria Milan",
+  githubUser: "victoriamilans",
+  linkedinUser: "victoriapmilan",
+  whatsappNumber: "+5519989484772",
+  emailUser: "victoria-milan@outlook.com",
+  instagramUser: "not.find",
   facebookUser: "",
 };
